1b9eb4713d273909dd61869748e81e95cf180273
Alex Rocha <rocha8524@gmail.com>
2020-04-21 15:55:22 -0400

    Navbar completed

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
 import React from "react";
 import "./App.css";
+import { Route, Switch } from "react-router-dom";
 
+// Pages
 import Home from "./pages/Home";
 import Rooms from "./pages/Rooms";
 import SingleRooms from "./pages/SingleRooms";
 import Error from "./pages/Error";
 
-function App() {
+// Components
+import NavBar from "./components/NavBar";
+
+const App = () => {
   return (
     <div>
-      <Home />
-      <Rooms />
-      <SingleRooms />
-      <Error />
+      <NavBar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/rooms" component={Rooms} />
+        <Route exact path="/rooms/:single" component={SingleRooms} />
+        <Route path="/error" component={Error} />
+      </Switch>
     </div>
   );
-}
+};
 
 export default App;
--- src/pages/SingleRooms.js
+++ src/pages/SingleRooms.js
@@ -1,11 +1,11 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-export default class SingleRooms extends Component {
-    render() {
-        return (
-            <div>
-                <h1>Single Room Page</h1>
-            </div>
-        )
-    }
+const SingleRooms = () => {
+    return (
+        <div>
+            <h1>Single Room Page</h1>
+        </div>
+    )
 }
+
+export default SingleRooms;
